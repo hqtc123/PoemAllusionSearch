b5cc4524bdbe1f8ae288e361ef59bbada2cbd8fe
hqtc123 <hqdo@foxmail.com>
2016-02-29 19:25:11 +0800

    inverted index build

--- inverted_index.py
+++ inverted_index.py
@@ -1,1 +1,61 @@
 __author__ = 'Qing'
+# -*- coding: UTF-8 -*-
+from bson.code import Code
+from pymongo import MongoClient
+from datetime import *
+
+mapper = Code("""
+    function(){
+        for(var i=0; i< this.content_arr.length; i++){
+            sentence = this.content_arr[i];
+            if(sentence.length<2){
+                continue;
+            }
+            var reg = /^.*[~!“’□《》（）@#\$%\^&\*\(\)_+\-=\[\]\{\}\\\|\'\";:,\<\.\>\/\?\s+].*$/;
+            if(reg.test(sentence)){
+                continue;
+            }
+            if(sentence.match(/\d/)!=null || sentence.match(/[a-zA-Z]/)!=null ){
+                continue;
+            }
+
+            sentence_start = this.content.indexOf(sentence)
+            for(var idx = 0;idx <sentence.length-1 ; idx++) {
+                word = sentence.substring(idx,idx+2);
+                value = {arr:[{"position":idx+sentence_start,"sentence_index":i, "poem_id":this._id}]}
+                emit(word,value);
+            }
+
+            if(sentence.length >2){
+                for(var idx = 0;idx <sentence.length-2; idx++) {
+                    word = sentence.substring(idx,idx+3);
+                    value = {arr:[{"position":idx+sentence_start,"sentence_index":i, "poem_id":this._id}]}
+                    emit(word,value);
+                }
+            }
+        };
+    }
+""")
+
+reducer = Code("""
+    function(key, values){
+        reduced = {arr :[],count:0}
+        for (var i =0;i<values.length; i++){
+            for(j=0; j< values[i].arr.length;j++){
+                reduced.arr.push(values[i].arr[j]);
+            }
+        }
+        return reduced;
+    }
+""")
+
+if __name__ == "__main__":
+    client = MongoClient("127.0.0.1", 27017)
+    db = client.ts_db
+    print("start time " + str(datetime.today()))
+    result = db.ts_co2.map_reduce(mapper, reducer, "inverted_index")
+    #
+    # for doc in result.find():
+    #     print(doc)
+
+    print("end time " + str(datetime.today()))
--- test.py
+++ test.py
@@ -7,21 +7,10 @@ from datetime import *
 if __name__ == "__main__":
     client = MongoClient("121.42.37.65", 27017)
     db = client.ts_db
-    old_collection = db.ts_collection
-    new_collection = db.ts_co666
-    print(old_collection.count())
-    print("start time " + str(datetime.today()))
-
-    for doc in old_collection.find({"zz": "李世民"}):
-        if doc["pm"] == "【句】":
-            continue
-        content = doc["zw"]
-        poem_dict = {
-            "title": doc["pm"][1:-1],
-            "author": doc["zz"],
-            "category": doc["bh"],
-            "content_arr": re.split("，|。|\.|\*|？|！|；", content)[:-1],
-            "content": content
-        }
-        new_collection.insert_one(poem_dict)
-    print("end time " + str(datetime.today()))
+    collection = db.myresults
+    doc = collection.find_one({"_id": "一半"})
+    print(str(doc["value"]["data"]["position"]) + "  sentence " + str(doc["value"]["data"]["sentence_index"]))
+    poem = db.ts_co666.find_one({"_id":doc["value"]["data"]["poem_id"]})
+    print(doc["value"]["data"]["poem_id"])
+    print(poem["content"])
+    print(poem["content_arr"])
